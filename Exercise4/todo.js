1bc7519bab7bfab7890ea830c0ee69bb3db0a9f8
mors <seyedsahel1383@gmail.com>
2025-12-08 22:09:55 +0330

    complete(without local storage)

--- Exercise4/todo.js
+++ Exercise4/todo.js
@@ -1,8 +1,14 @@
+
+let todos = [];
 document.querySelector('.js-addTodo').addEventListener('click',()=>{
    let todoElm = document.querySelector('.js-text');
    let todo = todoElm.value;
-   if (todo!== ''){
-    addTodo(todo);
+   if (todo !== ''){
+    todos.push({
+        name: todo,
+        done: false,
+    });
+    showTodo(todos);
     todoElm.value = '';
    }
 
@@ -12,44 +18,55 @@ document.body.addEventListener('keydown',(event)=>{
     if(event.key==='Enter'){
         let todoElm = document.querySelector('.js-text');
         let todo = todoElm.value;
-        if (todo!== ''){
-            addTodo(todo);
-            todoElm.value = '';
-        }
+        if (todo !== ''){
+    todos.push({
+        name: todo,
+        done: false,
+    });
+    showTodo(todos);
+    todoElm.value = '';
+   }
+
     }
 });
-let id = 10;
-function addTodo(todo){
-    id = id+1;
-    document.querySelector('.js-todoItem').innerHTML +=
-    `<div class="flex flex-row mb-5">
-         <div class="js-todoContext flex-grow flex flex-row gap-3 mx-3">
-            <div data-id="${id}" onclick="markAsDone(${id})" class="js-doneButton w-8 h-8 border-2 border-gray-400 rounded-full">
-            </div>
-            <div class="mt-1">
-                ${todo}
-            </div>
-        </div>
-        <div class="w-5 mx-5">
-            <img class="" src="assets/xmark.png" alt="">
-        </div>
-      </div>`
-}
 
-function markAsDone(id){
-    document.querySelectorAll('.js-doneButton')
-        .forEach((value) => {
-            if(Number(value.dataset.id)===id){
-                if(value.innerHTML === ""){
-                    value.innerHTML =
-                        `<img class="" src="assets/checkmark.png" alt="">`;
-                }else{
-                    value.innerHTML = "";
-                }
-                
-            }
-
-        })
+function showTodo(todos){
+    let todoDiv = document.querySelector('.js-todoItem');
+    todoDiv.innerHTML = "";
+    for (let i = 0; i < todos.length; i++) {
+    console.log(todos[i].done);
+         todoDiv.innerHTML +=
+            `<div data-id="${i}" class="js-todoContext flex flex-row mb-5">
+                <div class="flex-grow flex flex-row gap-3 mx-3">
+                    <div onclick="markAsDone(${i})" class="js-doneButton w-8 h-8 border-2 border-gray-400 rounded-full">
+                        ${todos[i].done ? `<img src="assets/checkmark.png">` : ""}
+                    </div>
+                    <div class="js-todoText mt-1 ${todos[i].done ? 'line-through' : ""}">
+                        ${todos[i].name}
+                    </div>
+                </div>
+                <div onclick="deleteTodo(${i})" class="js-delete w-5 mx-5">
+                    <img class="" src="assets/xmark.png" alt="">
+                </div>
+            </div>`
+    }
    
 }
 
+function markAsDone(id){
+    if(!todos[id].done){
+        todos[id].done = true;
+        showTodo(todos);
+        console.log(value.done,'if');
+    }else{
+        todos[id].done = false;
+        showTodo(todos);    
+        console.log(value.done,'else');
+
+    }
+}
+
+function deleteTodo(id){  
+    todos.splice(id,1);
+    showTodo(todos);
+}
