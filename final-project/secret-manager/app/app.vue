e3e9456c01394dc274f5636622015ef7a1aeaca1
mors <seyedsahel1383@gmail.com>
2025-12-15 16:27:38 +0330

    project structure

---
+++ final-project/secret-manager/app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout>
+      <div class="text-amber-300 text-8xl">
+        test
+      </div>
+  </NuxtLayout>
+</template>
---
+++ final-project/secret-manager/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="min-h-screen bg-gray-400">
+    <div>test</div>
+    <slot />
+  </div>
+</template>
